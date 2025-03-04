24bed730a9fd9323bf0922498f2dd7e1bdf45173
Ivan Smirnov (icy0o) <chaekplayyt@gmail.com>
2025-03-04 14:37:40 +0300

    append buffer

---
+++ src/append_buffer.c
@@ -0,0 +1,5 @@
+//
+// Created by ivan on 04.03.25.
+//
+
+#include "append_buffer.h"
---
+++ src/include/append_buffer.h
@@ -0,0 +1,8 @@
+//
+// Created by ivan on 04.03.25.
+//
+
+#ifndef APPEND_BUFFER_H
+#define APPEND_BUFFER_H
+
+#endif //APPEND_BUFFER_H
