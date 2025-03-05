1d2ea28043aee8332d1bfcac0bc059a6206fce16
Ivan Smirnov (icy0o) <chaekplayyt@gmail.com>
2025-03-05 16:15:35 +0300

    add home, pgup, pgdown, end, del keys

--- src/editor.c
+++ src/editor.c
@@ -14,7 +14,12 @@ enum editorKey {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    DELETE,
+    HOME,
+    END,
+    PAGE_UP,
+    PAGE_DOWN,
 };
 
 int editorReadKey() {
@@ -27,13 +32,38 @@ int editorReadKey() {
         char seq[3];
 
         if (read(STDIN_FILENO, &seq[0], 1) != 1 || read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
-
         if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '1': return HOME;
+                        case '3': return DELETE;
+                        case '4': return END;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME;
+                        case '8': return END;
+                        default: break;
+                    }
+                }
+            }
+            else {
+                switch (seq[1]) {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                    case 'H': return HOME;
+                    case 'F': return END;
+                    default: break;
+                }
+            }
+        }
+        else if (seq[0] == 'O') {
             switch (seq[1]) {
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+                case 'H': return HOME;
+                case 'F': return END;
                 default: break;
             }
         }
@@ -45,16 +75,20 @@ int editorReadKey() {
 void editorMoveCursor(const int key) {
     switch (key) {
         case ARROW_LEFT:
-            editor.cursorX--;
+            if (editor.cursorX != 0)
+                editor.cursorX--;
             break;
         case ARROW_UP:
-            editor.cursorY--;
+            if (editor.cursorY != 0)
+                editor.cursorY--;
             break;
         case ARROW_DOWN:
-            editor.cursorY++;
+            if (editor.cursorY != editor.rows - 1)
+                editor.cursorY++;
             break;
         case ARROW_RIGHT:
-            editor.cursorX++;
+            if (editor.cursorX != editor.cols - 1)
+                editor.cursorX++;
             break;
         default:
             break;
@@ -68,6 +102,20 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+        case HOME:
+            editor.cursorX = 0;
+            break;
+        case END:
+            editor.cursorX = editor.cols - 1;
+            break;
+        case PAGE_UP:
+        case PAGE_DOWN: {
+            int times = editor.rows;
+            while (times--) {
+                editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+            break;
+        }
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_LEFT:
